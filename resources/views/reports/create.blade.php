1b2581b02610a9900e245cf2f4b6d3367ca4d415
lem93 <lem.espinosa.m@gmail.com>
2016-05-05 00:12:47 -0600

    create reports, images and validation

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -21,14 +21,15 @@
 						Report info:
 					</header>
 					<div class="card-block">
-						<form method="POST" action="{{ url('reports') }}">
+						<form method="POST" action="{{ url('reports') }}" enctype="multipart/form-data">
 							{{ csrf_field() }}
 							<div class="form-group row {{($errors->has('completed_at'))? 'form-group-error':''}}">
 								<label class="col-sm-2 form-control-label">Compleated at:</label>
 								<div class="col-sm-10">
 									<div class='input-group date' id="new_report_datepicker">
 										<input type='text' name='completed_at' class="form-control"
-												id="new_report_datepicker_input" value="{{ old('completed_at') }}" />
+												id="new_report_datepicker_input" 
+												value="{{ old('completed_at') }}" required/>
 										@if ($errors->has('completed_at'))
 											<small class="text-muted">{{ $errors->first('completed_at') }}</small>
 										@endif
@@ -335,6 +336,79 @@
 										<small class="text-muted">{{ $errors->first('salt') }}</small>
 									@endif
 								</div>
+							</div>
+							<br>
+							<div class="form-group row">
+								<label class="col-sm-3 form-control-label">Photo 1 (Full Pool)</label>
+								<div class="col-sm-9">
+					                <div class="fileupload fileupload-new" data-provides="fileupload">
+					                  <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
+					                  <img src="{{ url('img/no_image.png') }}" alt="Placeholder" /></div>
+					                  <div class="fileupload-preview fileupload-exists thumbnail"
+					                   		style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
+					                  @if ($errors->has('photo1'))
+					                  	<br>
+										<span class="label label-danger">{{ $errors->first('photo1') }}</span>
+										<br><br>
+									  @endif
+					                  <div>
+					                    <span class="btn btn-default btn-file">
+					                    <span class="fileupload-new">Select image</span>
+					                    <span class="fileupload-exists">Change</span>
+					                    <input type="file" name="photo1" id="photo1" required></span>
+					                    <a href="#" class="btn btn-default fileupload-exists" 
+					                    	data-dismiss="fileupload">Remove</a>
+					                  </div>
+					                </div>
+				              	</div>
+							</div>
+							<div class="form-group row">
+								<label class="col-sm-3 form-control-label">Photo 2 (Water Quality)</label>
+								<div class="col-sm-9">
+					                <div class="fileupload fileupload-new" data-provides="fileupload">
+					                  <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
+					                  <img src="{{ url('img/no_image.png') }}" alt="Placeholder" /></div>
+					                  <div class="fileupload-preview fileupload-exists thumbnail"
+					                   		style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
+					                  @if ($errors->has('photo2'))
+					                  	<br>
+										<span class="label label-danger">{{ $errors->first('photo2') }}</span>
+										<br><br>
+									  @endif
+					                  <div>
+					                    <span class="btn btn-default btn-file">
+					                    <span class="fileupload-new">Select image</span>
+					                    <span class="fileupload-exists">Change</span>
+					                    <input type="file" name="photo2" id="photo2" required></span>
+					                    <a href="#" class="btn btn-default fileupload-exists" 
+					                    	data-dismiss="fileupload">Remove</a>
+					                  </div>
+					                </div>
+				              	</div>
+							</div>
+							<div class="form-group row">
+								<label class="col-sm-3 form-control-label">Photo 3 (Engine Room)</label>
+								<div class="col-sm-9">
+					                <div class="fileupload fileupload-new" data-provides="fileupload">
+					                  <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
+					                  <img src="{{ url('img/no_image.png') }}" alt="Placeholder" /></div>
+					                  <div class="fileupload-preview fileupload-exists thumbnail"
+					                   		style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
+					                  @if ($errors->has('photo3'))
+					                  	<br>
+										<span class="label label-danger">{{ $errors->first('photo3') }}</span>
+										<br><br>
+									  @endif
+					                  <div>
+					                    <span class="btn btn-default btn-file">
+					                    <span class="fileupload-new">Select image</span>
+					                    <span class="fileupload-exists">Change</span>
+					                    <input type="file" name="photo3" id="photo3" required></span>
+					                    <a href="#" class="btn btn-default fileupload-exists" 
+					                    	data-dismiss="fileupload">Remove</a>
+					                  </div>
+					                </div>
+				              	</div>
 							</div>
 							<hr>
 							<p style="float: left;">
