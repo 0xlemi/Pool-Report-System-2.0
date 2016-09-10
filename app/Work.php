2009eaa27b9f83f3da9c3a2f1564faa163bdf203
lem93 <lem.espinosa.m@gmail.com>
2016-09-09 19:20:33 -0600

    work and workOrder models

--- app/Work.php
+++ app/Work.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Work extends Model
 {
-    //
+    use ImageTrait;
+
+    /**
+     * variables that can be mass assigned
+     * @var array
+     */
+    protected $fillable = [
+        'description',
+        'quantity',
+        'units',
+        'cost',
+        'work_order_id',
+        'technician_id',
+    ];
+
+    /**
+     * associated workOrder with this work
+     */
+    public function workOrder()
+    {
+    	return $this->belongsTo('App\WorkOrder')->first();
+    }
+
+    /**
+     * associated technician with this work
+     */
+    public function technician()
+    {
+    	return $this->belongsTo('App\Technician')->first();
+    }
+
+    /**
+     * associated service with this work
+     */
+    public function service()
+    {
+        return $this->workOrder()->service();
+    }
 }
--- app/WorkOrder.php
+++ app/WorkOrder.php
@@ -4,7 +4,49 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\PRS\Traits\Model\ImageTrait;
+
 class WorkOrder extends Model
 {
-    //
+
+    use ImageTrait;
+
+    /**
+     * variables that can be mass assigned
+     * @var array
+     */
+    protected $fillable = [
+        'start',
+        'end',
+        'finished',
+        'price',
+        'currency',
+        'service_id',
+        'supervisor_id',
+    ];
+
+    /**
+     * associated service with this workOrder
+     */
+    public function service()
+    {
+    	return $this->belongsTo('App\Service')->first();
+    }
+
+    /**
+     * associated supervisor with this workOrder
+     */
+    public function supervisor()
+    {
+    	return $this->belongsTo('App\Supervisor')->first();
+    }
+
+    /**
+     * Get associated works with this work Order
+     */
+    public function works()
+    {
+    	return $this->hasMany('App\Work');
+    }
+
 }
