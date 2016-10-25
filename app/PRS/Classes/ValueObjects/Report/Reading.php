b1c15b89ec55955ae5aaf07f125c6e4cabdfc87c
lem93 <lem.espinosa.m@gmail.com>
2016-10-25 01:25:49 -0600

    tests for Reading Class

--- app/PRS/Classes/ValueObjects/Report/Reading.php
+++ app/PRS/Classes/ValueObjects/Report/Reading.php
@@ -8,6 +8,9 @@ class Reading{
 
     protected $reading;
     protected $tag;
+    protected $classes = [ 1 =>'info', 2 => 'primary',
+                        3 => 'success', 4 => 'warning',
+                        5 => 'danger'];
 
     public function __construct(int $reading, Tag $tag)
     {
@@ -15,6 +18,30 @@ class Reading{
         $this->tag = $tag;
     }
 
+    /**
+     * Get the class color for depending on the reading
+     * @return string      class name
+     * tested
+     */
+    public function class()
+    {
+        return $this->classes[$this->reading];
+    }
+
+    /**
+     * Get styled html span
+     * @return string
+     * tested
+     */
+    public function styled()
+    {
+		return "<span class=\"label label-{$this->class()}\">{$this}</span>";
+    }
+
+    /**
+     * @return string
+     * tested
+     */
     public function __toString()
     {
         return $this->tag->fromReading($this->reading);
---
+++ tests/unit/ValueObjects/Report/ReadingTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\PRS\Classes\ValueObjects\Report\Reading;
+
+class ReadingTest extends TestCase
+{
+
+    /** @test */
+    public function get_reading_to_string()
+    {
+        // Given
+        $tagMock = Mockery::mock('App\PRS\Classes\ValueObjects\Administrator\Tag');
+        $tagMock->shouldReceive('fromReading')
+                ->once()
+                ->andReturn('reading');
+        $reading = new Reading(1, $tagMock);
+
+        // When
+        $string = (string) $reading;
+
+        // Then
+        $this->assertEquals($string, 'reading');
+
+    }
+
+    /** @test */
+    public function get_reading_class_color()
+    {
+        // Given
+        $tagMock = Mockery::mock('App\PRS\Classes\ValueObjects\Administrator\Tag');
+        $readingVeryLow = new Reading(1, $tagMock);
+        $readingPerfect = new Reading(3, $tagMock);
+        $readingVeryHigh = new Reading(5, $tagMock);
+
+        // When
+        $info = $readingVeryLow->class();
+        $success = $readingPerfect->class();
+        $danger = $readingVeryHigh->class();
+
+        // Then
+        $this->assertEquals($info, 'info');
+        $this->assertEquals($success, 'success');
+        $this->assertEquals($danger, 'danger');
+
+    }
+
+    /** @test */
+    public function get_styled_reading_html_span()
+    {
+        // Given
+        $tagMock = Mockery::mock('App\PRS\Classes\ValueObjects\Administrator\Tag');
+        $readingMock = Mockery::mock('App\PRS\Classes\ValueObjects\Report\Reading[class,toString]', [0, $tagMock]);
+        $readingMock->shouldReceive('class')
+                    ->once()
+                    ->andReturn('class');
+        $readingMock->shouldReceive('__toString')
+                    ->once()
+                    ->andReturn('text');
+
+        // When
+        $span = $readingMock->styled();
+
+        // Then
+        $this->assertEquals($span, '<span class="label label-class">text</span>');
+
+    }
+
+}
