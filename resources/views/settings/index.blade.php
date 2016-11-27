2dc3dd3768ddf2887cc2fefa13c7e0dc2de4f209
lem93 <lem.espinosa.m@gmail.com>
2016-11-26 21:10:20 -0700

    make settings more pretty

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -3,121 +3,72 @@
 @section('content')
 <div class="row settingsVue">
 
-    <div class="col-lg-3 col-md-6 col-sm-12">
-        <section class="box-typical">
-            <div class="profile-card">
-                <div class="profile-card-photo">
-                    <img src="img/photo-220-1.jpg" alt=""/>
-                </div>
-                <div class="profile-card-name">@{{ objectName+' '+objectLastName }}</div>
-                <div class="profile-card-status">@{{ companyName }}</div>
-                <div class="profile-card-location">Business Plan</div>
-
-            </div><!--.profile-card-->
-
-            <div class="profile-statistic tbl">
-                <div class="tbl-row">
-                    <div class="tbl-cell">
-                        <b>{{ $user->admin()->technicians()->get()->count() }}</b>
-                        Technicians
-                    </div>
-                    <div class="tbl-cell">
-                        <b>{{ $user->admin()->reports()->get()->count() }}</b>
-                        Reports done
-                    </div>
-                </div>
-            </div>
-                <ul class="profile-links-list">
-                    <li class="nowrap">
-                        <i class="font-icon font-icon-mail"></i>
-                        &nbsp;{{ $user->email }}
-                    </li>
-                    <li class="nowrap">
-                        <i class="font-icon font-icon-earth-bordered"></i>
-                        <a href="http://@{{ website }}">@{{ website }}</a>
-                    </li>
-                    <li class="nowrap">
-                        <i class="font-icon font-icon-fb-fill"></i>
-                        <a href="http://www.facebook.com/@{{ facebook }}">facebook.com/@{{ facebook }}</a>
-                    </li>
-                    <li class="nowrap">
-                        <i class="font-icon font-icon-tw-fill"></i>
-                        <a href="http://www.twitter.com/@{{ twitter }}">twitter.com/@{{ twitter }}</a>
-                    </li>
-                </ul>
-        </section><!--.box-typical-->
-    </div><!--.col- -->
+    @include('settings.profile')
 
     <div class="col-lg-9 col-md-12 col-sm-12">
-
-        <section class="box-typical">
-            <header class="box-typical-header-sm">Settings</header>
-            <br>
             <section class="tabs-section">
-                <div class="tabs-section-nav tabs-section-nav-icons">
+                <div class="tabs-section-nav">
                     <div class="tbl">
                         <ul class="nav" role="tablist">
-                            @can('account', $setting)
+
                             <li class="nav-item">
-                                <a class="nav-link active" href="#tabs-1-tab-2" role="tab" data-toggle="tab">
+                                <a class="nav-link active" href="#tabs-1-tab-1" role="tab" data-toggle="tab">
                                     <span class="nav-link-in">
-                                        <span class="font-icon font-icon-user"></span>
-                                        Account
+                                        <i class="font-icon font-icon-user"></i>&nbsp;
+                                        Profile
                                     </span>
                                 </a>
                             </li>
-                            @endcan
-                            @can('company', $setting)
+
                             <li class="nav-item">
-                                <a class="nav-link" href="#tabs-1-tab-1" role="tab" data-toggle="tab">
+                                <a class="nav-link" href="#tabs-1-tab-2" role="tab" data-toggle="tab">
                                     <span class="nav-link-in">
-                                        <i class="font-icon font-icon-build"></i>
-                                        Company Profile
+                                        <i class="font-icon font-icon-build"></i>&nbsp;
+                                        Customization
                                     </span>
                                 </a>
                             </li>
-                            @endcan
-                            @can('email', $setting)
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#tabs-1-tab-3" role="tab" data-toggle="tab">
+                                    <span class="nav-link-in">
+                                        <i class="font-icon font-icon-mail"></i>&nbsp;
+                                        Notifications
+                                    </span>
+                                </a>
+                            </li>
+
                             <li class="nav-item">
                                 <a class="nav-link" href="#tabs-1-tab-4" role="tab" data-toggle="tab">
                                     <span class="nav-link-in">
-                                        <i class="font-icon font-icon-mail"></i>
-                                        Email
-                                    </span>
-                                </a>
-                            </li>
-                            @endcan
-                            @can('billing', $setting)
-                            <li class="nav-item">
-                                <a class="nav-link" href="#tabs-1-tab-5" role="tab" data-toggle="tab">
-                                    <span class="nav-link-in">
-                                        <i class="glyphicon glyphicon-credit-card"></i>
+                                        <i class="glyphicon glyphicon-credit-card"></i>&nbsp;
                                         Billing
                                     </span>
                                 </a>
                             </li>
-                            @endcan
-                            @can('permissions', $setting)
+
                             <li class="nav-item">
-                                <a class="nav-link" href="#tabs-1-tab-6" role="tab" data-toggle="tab">
+                                <a class="nav-link" href="#tabs-1-tab-5" role="tab" data-toggle="tab">
                                     <span class="nav-link-in">
-                                        <i class="font-icon font-icon-lock"></i>
+                                        <i class="font-icon font-icon-lock"></i>&nbsp;
                                         Permissions
                                     </span>
                                 </a>
                             </li>
-                            @endcan
+
                         </ul>
                     </div>
                 </div><!--.tabs-section-nav-->
 
                 <div class="tab-content">
 
-
-                    @can('account', $setting)
-                    <div role="tabpanel" class="tab-pane fade in active" id="tabs-1-tab-2">
+                    <!-- Profile     -->
+                    <div role="tabpanel" class="tab-pane fade in active" id="tabs-1-tab-1">
                         <br>
                         @include('settings.account')
+
+                        <hr>
+                        @include('settings.company')
 
                         <hr />
                         @include('settings.changeEmail')
@@ -129,35 +80,29 @@
                       <br>
 
                     </div><!--.tab-pane-->
-                    @endcan
 
-                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-1">
-                        <br>
-                        @include('settings.company')
+                    <!-- System Settings -->
+                    <div role="tabpanel" class="tab-pane fade in active" id="tabs-1-tab-2">
+
                     </div><!--.tab-pane-->
 
-                    @can('email', $setting)
-                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-4">
+                    <!-- Notifications -->
+                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-3">
                         @include('settings.email')
                     </div><!--.tab-pane-->
-                    @endcan
 
-                    @can('billing', $setting)
-                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-5">
-                        @include('settings.billing')    
+                    <!-- Billing -->
+                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-4">
+                        @include('settings.billing')
                     </div><!--.tab-pane-->
-                    @endcan
 
-                    @can('permissions', $setting)
-                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-6">
+                    <!-- Permissions -->
+                    <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-5">
                         @include('settings.permissions')
                     </div><!--.tab-pane-->
-                    @endcan
 
                 </div><!--.tab-content-->
             </section><!--.tabs-section-->
-
-        </section><!--.box-typical-->
     </div><!--.col- -->
 
 
---
+++ resources/views/settings/profile.blade.php
@@ -0,0 +1,44 @@
+<div class="col-lg-3 col-md-6 col-sm-12">
+    <section class="box-typical">
+        <div class="profile-card">
+            <div class="profile-card-photo">
+                <img src="img/photo-220-1.jpg" alt=""/>
+            </div>
+            <div class="profile-card-name">@{{ objectName+' '+objectLastName }}</div>
+            <div class="profile-card-status">@{{ companyName }}</div>
+            <div class="profile-card-location">Business Plan</div>
+
+        </div><!--.profile-card-->
+
+        <div class="profile-statistic tbl">
+            <div class="tbl-row">
+                <div class="tbl-cell">
+                    <b>{{ $user->admin()->technicians()->get()->count() }}</b>
+                    Technicians
+                </div>
+                <div class="tbl-cell">
+                    <b>{{ $user->admin()->reports()->get()->count() }}</b>
+                    Reports done
+                </div>
+            </div>
+        </div>
+            <ul class="profile-links-list">
+                <li class="nowrap">
+                    <i class="font-icon font-icon-mail"></i>
+                    &nbsp;{{ $user->email }}
+                </li>
+                <li class="nowrap">
+                    <i class="font-icon font-icon-earth-bordered"></i>
+                    <a href="http://@{{ website }}">@{{ website }}</a>
+                </li>
+                <li class="nowrap">
+                    <i class="font-icon font-icon-fb-fill"></i>
+                    <a href="http://www.facebook.com/@{{ facebook }}">facebook.com/@{{ facebook }}</a>
+                </li>
+                <li class="nowrap">
+                    <i class="font-icon font-icon-tw-fill"></i>
+                    <a href="http://www.twitter.com/@{{ twitter }}">twitter.com/@{{ twitter }}</a>
+                </li>
+            </ul>
+    </section><!--.box-typical-->
+</div><!--.col- -->
