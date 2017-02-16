9aaefb4e2d4c566766b4d5c0fbdf925c272ba933
lem93 <lem.espinosa.m@gmail.com>
2017-02-16 13:31:39 -0700

    organize routes api

--- routes/api.php
+++ routes/api.php
@@ -17,65 +17,95 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // })->middleware('auth:api');
 
+// Authentication
 Route::group(['middleware' => ['throttle:10'] ], function (){
 	Route::post('login', 'Api\UserController@login');
 	Route::post('signup', 'Api\AdministratorsController@store');
 });
 
 Route::group(['middleware' => ['auth:api'] ], function(){
+
+
+	// User
 	Route::get('todaysroute', 'Api\UserController@todaysRoute');
 	Route::post('resettoken', 'Api\UserController@resetToken');
 	Route::get('account', 'Api\UserController@show');
 	Route::post('account', 'Api\UserController@update');
 
+
+	// Administrator
 	Route::post('permissions', 'Api\AdministratorsController@permissions');
 
-	// Contract
-	Route::get('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@show');
-	Route::post('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@storeOrUpdate');
-	Route::delete('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@destroy');
 
-	// Equipment
-	Route::get('services/{serviceSeqId}/equipment', 'Api\EquipmentController@index');
-	Route::post('services/{serviceSeqId}/equipment', 'Api\EquipmentController@store');
-	Route::get('equipment/{equipment}', 'Api\EquipmentController@show');
-	Route::post('equipment/{equipment}', 'Api\EquipmentController@update');
-	Route::delete('equipment/{equipment}', 'Api\EquipmentController@destroy');
+	// Reports
+	Route::resource('reports', 'Api\ReportsController', ['except' => [
+		'create', 'edit'
+	]]);
 
-	// Chemicals
-	Route::get('services/{serviceSeqId}/chemicals', 'Api\ChemicalController@index');
-	Route::post('services/{serviceSeqId}/chemicals', 'Api\ChemicalController@store');
-	Route::get('chemicals/{chemical}', 'Api\ChemicalController@show');
-	Route::post('chemicals/{chemical}', 'Api\ChemicalController@update');
-	Route::delete('chemicals/{chemical}', 'Api\ChemicalController@destroy');
 
-	// Work
-	Route::get('workorders/{workOrderSeqId}/work', 'Api\WorkController@index');
-	Route::post('workorders/{workOrderSeqId}/work', 'Api\WorkController@store');
-	Route::get('work/{work}', 'Api\WorkController@show');
-	Route::post('work/{work}', 'Api\WorkController@update');
-	Route::delete('work/{work}', 'Api\WorkController@destroy');
-
-	// WorkOrder
-	Route::resource('workorders', 'Api\WorkOrderController');
+	// Work Order
+	Route::resource('workorders', 'Api\WorkOrderController', ['except' => [
+		'create', 'edit'
+	]]);
 	Route::post('workorders/{seq_id}/finish', 'Api\WorkOrderController@finish');
+		// Work
+		Route::get('workorders/{workOrderSeqId}/work', 'Api\WorkController@index');
+		Route::post('workorders/{workOrderSeqId}/work', 'Api\WorkController@store');
+		Route::get('work/{work}', 'Api\WorkController@show');
+		Route::post('work/{work}', 'Api\WorkController@update');
+		Route::delete('work/{work}', 'Api\WorkController@destroy');
 
-	// Payments
-	Route::get('invoices/{invoiceSeqId}/payments', 'Api\PaymentController@index');
-	Route::post('invoices/{invoiceSeqId}/payments', 'Api\PaymentController@store');
 
-	// Work
-	// Route::get('work/{work}', 'Api\WorkController@show');
+	// Service
+	Route::resource('services', 'Api\ServicesController', ['except' => [
+		'create', 'edit'
+	]]);
+		// Contract
+		Route::get('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@show');
+		Route::post('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@storeOrUpdate');
+		Route::delete('services/{serviceSeqId}/contract', 'Api\ServiceContractsController@destroy');
+		// Equipment
+		Route::get('services/{serviceSeqId}/equipment', 'Api\EquipmentController@index');
+		Route::post('services/{serviceSeqId}/equipment', 'Api\EquipmentController@store');
+		Route::get('equipment/{equipment}', 'Api\EquipmentController@show');
+		Route::post('equipment/{equipment}', 'Api\EquipmentController@update');
+		Route::delete('equipment/{equipment}', 'Api\EquipmentController@destroy');
+		// Chemicals
+		Route::get('services/{serviceSeqId}/chemicals', 'Api\ChemicalController@index');
+		Route::post('services/{serviceSeqId}/chemicals', 'Api\ChemicalController@store');
+		Route::get('chemicals/{chemical}', 'Api\ChemicalController@show');
+		Route::post('chemicals/{chemical}', 'Api\ChemicalController@update');
+		Route::delete('chemicals/{chemical}', 'Api\ChemicalController@destroy');
 
-	Route::resource('services', 'Api\ServicesController');
-	Route::resource('supervisors', 'Api\SupervisorsController');
-	Route::resource('technicians', 'Api\TechniciansController');
-	Route::resource('clients', 'Api\ClientsController');
-	Route::resource('reports', 'Api\ReportsController');
+
+	// Client
+	Route::resource('clients', 'Api\ClientsController', ['except' => [
+		'create', 'edit'
+	]]);
+
+
+	// Supervisor
+	Route::resource('supervisors', 'Api\SupervisorsController', ['except' => [
+		'create', 'edit'
+	]]);
+
+
+	// Technician
+	Route::resource('technicians', 'Api\TechniciansController', ['except' => [
+		'create', 'edit'
+	]]);
+
+
+	// Invoces
 	Route::resource('invoices', 'Api\InvoiceController', ['only' => [
 	    'index', 'show', 'destroy'
 	]]);
-	Route::resource('payments', 'Api\PaymentController', ['only' => [
-	    'show', 'destroy'
-	]]);
+		// Payments
+		Route::resource('payments', 'Api\PaymentController', ['only' => [
+		    'show', 'destroy'
+		]]);
+		Route::get('invoices/{invoiceSeqId}/payments', 'Api\PaymentController@index');
+		Route::post('invoices/{invoiceSeqId}/payments', 'Api\PaymentController@store');
+
+
 });
