688ec191fa964a3c928e0a4c8faf235ebc044def
lem93 <lem.espinosa.m@gmail.com>
2017-02-18 11:27:05 -0700

    api report authorization tested

--- tests/integration/controllers/Api/authorization/ReportAuthorizationTest.php
+++ tests/integration/controllers/Api/authorization/ReportAuthorizationTest.php
@@ -3,16 +3,347 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+use App\Administrator;
 
-class ReportAuthorizationTest extends TestCase
+class ReportAuthorizationTest extends ApiTester
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    protected $admin;
+    protected $sup;
+    protected $tech;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->withoutMiddleware();
+
+        $this->admin = factory(Administrator::class)->create();
+        $user = factory(User::class)->create([
+            'userable_id' => $this->admin->id,
+            'userable_type' => 'App\Administrator',
+        ]);
+
+        $service = $this->createService($this->admin->id);
+
+        $this->sup = $this->createSupervisor($this->admin->id);
+
+        $this->tech = $this->createTechnician($this->sup->id);
+
+        $this->createReport($service->id, $this->tech->id);
     }
+
+
+    //****************************************
+    //               LIST
+    //****************************************
+
+    /** @test */
+    public function it_authorizes_supervisor_to_list_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_view = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('GET', 'api/v1/reports');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_supervisor_to_list_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_view = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('GET', 'api/v1/reports');
+        $this->assertResponseStatus(403);
+
+    }
+
+    /** @test */
+    public function it_authorizes_technician_to_list_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_view = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('GET', 'api/v1/reports');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_technician_to_list_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_view = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('GET', 'api/v1/reports');
+        $this->assertResponseStatus(403);
+    }
+
+
+    //****************************************
+    //               VIEW
+    //****************************************
+
+    /** @test */
+    public function it_authorizes_supervisor_to_view_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_view = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('GET', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_supervisor_to_view_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_view = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('GET', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+
+    }
+
+    /** @test */
+    public function it_authorizes_technician_to_view_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_view = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('GET', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_technician_to_view_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_view = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('GET', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+    }
+
+
+    //****************************************
+    //               CREATE
+    //****************************************
+
+    /** @test */
+    public function it_authorizes_supervisor_to_create_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_create = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('POST', 'api/v1/reports');
+        $this->assertResponseStatus(422);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_supervisor_to_create_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_create = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('POST', 'api/v1/reports');
+        $this->assertResponseStatus(403);
+
+    }
+
+    /** @test */
+    public function it_authorizes_technician_to_create_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_create = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('POST', 'api/v1/reports');
+        $this->assertResponseStatus(422);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_technician_to_create_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_create = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('POST', 'api/v1/reports');
+        $this->assertResponseStatus(403);
+    }
+
+
+    //****************************************
+    //               UPDATE
+    //****************************************
+
+    /** @test */
+    public function it_authorizes_supervisor_to_update_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_update = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('PATCH', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_supervisor_to_update_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_update = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('PATCH', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+
+    }
+
+    /** @test */
+    public function it_authorizes_technician_to_update_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_update = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('PATCH', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200);
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_technician_to_update_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_update = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('PATCH', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+    }
+
+
+    //****************************************
+    //               ADD PHOTO
+    //****************************************
+
+    // missing tests
+
+    //****************************************
+    //               REMOVE PHOTO
+    //****************************************
+
+    // missing tests
+
+
+    //****************************************
+    //               DELETE
+    //****************************************
+
+    /** @test */
+    public function it_authorizes_supervisor_to_delete_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_delete = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('DELETE', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200); // because we removed the middleware
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_supervisor_to_delete_report()
+    {
+        // Given
+        // When
+        $this->admin->sup_report_delete = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->sup->user, 'api')
+            ->json('DELETE', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+
+    }
+
+    /** @test */
+    public function it_authorizes_technician_to_delete_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_delete = 1;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('DELETE', 'api/v1/reports/10001');
+        $this->assertResponseStatus(200); // because we removed the middleware
+
+    }
+
+    /** @test */
+    public function it_unauthorizes_technician_to_delete_report()
+    {
+        // Given
+        // When
+        $this->admin->tech_report_delete = 0;
+        $this->admin->save();
+        // Then
+        $this->actingAs($this->tech->user, 'api')
+            ->json('DELETE', 'api/v1/reports/10001');
+        $this->assertResponseStatus(403);
+    }
+
 }
