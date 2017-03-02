60561e5fd0285ff8a07d8f04f585bce117056c22
lem93 <lem.espinosa.m@gmail.com>
2017-03-01 21:08:07 -0700

    improve UI notifications

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -18,40 +18,45 @@
 	<div class="col-md-12 col-lg-12 col-xl-8 col-xl-offset-2">
         <section class="box-typical">
 			<div class="box-typical-body">
-                <table id="table-edit" class="table table-bordered table-hover">
-    				<thead>
-    				<tr>
-    					<th>Description</th>
-    					<th width="150">When</th>
-						<th class="table-icon-cell">
-							<i class="fa fa-photo"></i>
-						</th>
-    				</tr>
-    				</thead>
-    				<tbody>
-                        @foreach($notifications as $notification)
-    					<tr>
-    						<td class="color-blue-grey-lighter">
-								@if($notification->read_at === null)
-								<i class="fa fa-circle" style="color: #FA424A;"></i>
-								&nbsp;&nbsp;
-								@endif
-								{!! $notification->data['message'] !!}
-							</td>
-    						<td class="table-date">{{ $carbon::parse($notification->created_at)->diffForHumans() }}</td>
-							<td class="table-photo">
-								<img src="{{ $notification->data['icon'] }}" alt="" data-toggle="tooltip" data-placement="bottom" title="Photo">
-							</td>
-    					</tr>
-                        @endforeach
-    				</tbody>
-    			</table>
-            	<div class="col-md-12">
-					<nav>
-						{{ $notifications->links() }}
-						<all-notifications-as-read-button></all-notifications-as-read-button>
-					</nav>
-				</div>
+				@if(!$notifications->isEmpty())
+	                <table id="table-edit" class="table table-bordered table-hover">
+	    				<thead>
+	    				<tr>
+	    					<th>Description</th>
+	    					<th width="150">When</th>
+							<th class="table-icon-cell">
+								<i class="fa fa-photo"></i>
+							</th>
+	    				</tr>
+	    				</thead>
+	    				<tbody>
+	                        @foreach($notifications as $notification)
+	    					<tr>
+	    						<td class="color-blue-grey-lighter">
+									@if($notification->read_at === null)
+									<i class="fa fa-circle" style="color: #FA424A;"></i>
+									&nbsp;&nbsp;
+									@endif
+									{!! $notification->data['message'] !!}
+								</td>
+	    						<td class="table-date">{{ $carbon::parse($notification->created_at)->diffForHumans() }}</td>
+								<td class="table-photo">
+									<img src="{{ $notification->data['icon'] }}" alt="" data-toggle="tooltip" data-placement="bottom" title="Photo">
+								</td>
+	    					</tr>
+	                        @endforeach
+	    				</tbody>
+	    			</table>
+	            	<div class="col-md-12">
+						<nav>
+							{{ $notifications->links() }}
+							<all-notifications-as-read-button></all-notifications-as-read-button>
+						</nav>
+					</div>
+				@else
+					<br>
+            		<h4 style="text-align: center;padding: 60px 0;">There are no notifications yet.</h4>
+				@endif
         	</div><!--.box-typical-body-->
 		</section><!--.box-typical-->
 	</div>
