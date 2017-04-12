2844212d45300a067e87ba8331dd2c1ac7b0a0ad
lem93 <lem.espinosa.m@gmail.com>
2017-04-12 16:28:55 -0600

    add intercom to the landing page

--- resources/views/landing/foot.blade.php
+++ resources/views/landing/foot.blade.php
@@ -21,6 +21,13 @@
 <!-- Custom Script -->
 <script src="{{ url('js/custom.js') }}"></script>
 
+<script>
+  window.intercomSettings = {
+    app_id: "zcndm75f"
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/zcndm75f';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
 
 
 <iframe id="rufous-sandbox" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: none;"></iframe>
