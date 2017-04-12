2e9d38926c9a3a3d98a6a5275547efaf8188d1a5
lem93 <lem.espinosa.m@gmail.com>
2017-04-12 17:11:34 -0600

    add intercom to the web app too

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,14 @@
     </script>
     <script src="{{ url('js/plugins.js') }}"></script>
     <script src="{{ elixir('js/bundle.js') }}"></script>
+
+	<script>
+	  window.intercomSettings = {
+	    app_id: "zcndm75f"
+	  };
+	</script>
+	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/zcndm75f';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
     @include('layouts.footer')
 </body>
 </html>
