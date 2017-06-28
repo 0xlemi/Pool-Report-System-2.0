9703e0909337c2e41407c1b54d44af135e7e71ca
lem93 <lem.espinosa.m@gmail.com>
2017-06-28 09:57:48 -0600

    small pusher bug

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,9 +1,9 @@
 
 
-import Echo from "laravel-echo"
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'adaa734ec4ce7b21a7ae',
-    encrypted: true
-});
+// import Echo from "laravel-echo"
+//
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'adaa734ec4ce7b21a7ae',
+//     encrypted: true
+// });
