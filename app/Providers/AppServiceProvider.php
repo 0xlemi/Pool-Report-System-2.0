15f78666fa717e80f703028b88db1fa18df87768
lem93 <lem.espinosa.m@gmail.com>
2017-07-19 11:30:37 -0600

    force ssl in heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force Http in heroku
+        if(env('APP_ENV') == 'production'){
+            URL::forceScheme('https');
+        }
     }
 
     /**
