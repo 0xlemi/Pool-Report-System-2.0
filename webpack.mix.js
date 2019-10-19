cccac383a27167c7c065a95ff92890b9742a7b29
Luis Espinosa <lem.espinosa.m@gmail.com>
2019-10-18 20:36:15 -0600

    Webpack is compiling. Non of the js works jet but im getting green.

---
+++ webpack.mix.js
@@ -0,0 +1,50 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+// elixir(function(mix) {
+//     mix.less('app.less', 'public/css/less.css')
+//     	.styles([
+//     		'bootstrap-table.css',
+//             'font-awesome.min.css',
+//     		'dropzone.css',
+//             'bootstrap-fileupload.css',
+//             'bootstrap-toggle.css',
+//             'bootstrap-clockpicker.css',
+//             'sweetalert.css',
+//         	'custom.css'
+//     	], 'public/css/main.css')
+//     	.browserify([
+//             'bootstrap-table.js',
+//             'bootstrap-fileupload.js',
+//             'bootstrap-clockpicker.js',
+//             'bootstrap-maxlenght.js',
+//             'bootstrap.js',
+//             'main.js'
+//         ], 'public/js/bundle.js')
+//         // Documentation Styles
+//         .sass([
+//             'theDocs/theDocs.scss',
+//             'theDocs/custom.css'
+//         ], 'public/css/docs.css')
+//         .less('error.less', 'public/css/error.css')
+//     	.version(['css/less.css', 'css/main.css', 'js/bundle.js', 'css/docs.css']);
+// });
+
+// mix.extract([
+      // 'vue',
+      // 'jquery'
+  // ]);
+
+// mix.sass('resources/assets/sass/theDocs/theDocs.scss', 'public/css/docs.css');
+    // .sass('resources/sass/app.scss', 'public/css');
